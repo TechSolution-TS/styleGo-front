eda60f0ac0c07c9298b9f3af80b739b0485e38f1
Vitor Oliveira <chazan.uni@gmail.com>
2025-01-20 18:19:25 -0300

    Ajustes nas rotas da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BarberListingComponent } from "./barber-listing/barber-listing.component";
+import { RouterOutlet } from '@angular/router';
+import { BarberProfileComponent } from './pages/barber-profile/barber-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    HeaderComponent, 
-    BarberListingComponent],
+    HeaderComponent,
+    RouterOutlet,
+],
   templateUrl: './app.component.html',
   styleUrls: [
     './app.component.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BarberProfileComponent } from './pages/barber-profile/barber-profile.component';
 import { AppComponent } from './app.component';
+import { BarberListingComponent } from './barber-listing/barber-listing.component';
 
 export const routes: Routes = [
   { path: 'barber/:id', component: BarberProfileComponent },
-  { path: '', component: AppComponent},
-  //{ path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '', component: BarberListingComponent },   // Rota raiz para BarberListingComponent
+  { path: 'home', redirectTo: '', pathMatch: 'full' }, // Redireciona /home para /
+  { path: '**', redirectTo: '', pathMatch: 'full' }   // Redireciona qualquer outra rota para /
 ];
